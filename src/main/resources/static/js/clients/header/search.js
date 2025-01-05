a9fe4e49ac5006fb95b2f1ae067e60de35721811
TranHa9 <tranha.99884@gmail.com>
2025-01-05 14:14:43 +0700

    update

--- src/main/resources/static/js/clients/header/search.js
+++ src/main/resources/static/js/clients/header/search.js
@@ -4,12 +4,21 @@ $(document).ready(function () {
     let pageInfo;
     let pageSize = 10;
     let pageIndex = 0;
+
+    // Lấy searchData từ sessionStorage nếu đang ở trang /search
+    let searchData = {};
+    if (window.location.pathname === '/search') {
+        const savedData = sessionStorage.getItem('searchData');
+        searchData = savedData ? JSON.parse(savedData) : {};
+    }
+
     // Gọi API lấy danh mục
     $.ajax({
         url: '/api/v1/categories/active',
         type: 'GET',
         success: function (response) {
             const categorySelect = $('#category-select');
+            const categoryFillters = $(".sidebar-border .sidebar-content .list-nav-arrow")
             categorySelect.empty();
             categorySelect.append('<option value="">Tất cả</option>');
             response.forEach(function (category) {
@@ -17,6 +26,29 @@ $(document).ready(function () {
             });
             // Cập nhật lại select2 (nếu bạn đang dùng select2)
             categorySelect.select2();
+            categoryFillters.empty();
+            const mainCategories = response.slice(0, 5);
+            const extraCategories = response.slice(5);
+            categoryFillters.append(`
+                <li><a href="#">Tất cả<span class="number">${response.length}</span></a>
+                                </li>
+            `)
+            mainCategories.forEach(function (category) {
+                categoryFillters.append(`<li><a href="#">${category.name}<span class="number">09</span></a>
+                                        </li>
+                                 `);
+            });
+            const moreMenu = $("#moreMenu .list-nav-arrow");
+            moreMenu.empty();
+            extraCategories.forEach(function (category) {
+                moreMenu.append(`
+                <li><a href="#">${category.name}<span class="number"></span></a></li>
+            `);
+            });
+            // Gán giá trị nếu đang ở trang /search
+            if (window.location.pathname === '/search' && searchData?.categoryId) {
+                categorySelect.val(searchData.categoryId).trigger('change');
+            }
         }
     });
 
@@ -46,6 +78,7 @@ $(document).ready(function () {
         });
         console.log(data)
         if (window.location.pathname === '/search') {
+            sessionStorage.removeItem('searchData');
             getProductData(data);
         } else {
             // Nếu không, chuyển hướng sang trang /search
@@ -54,9 +87,11 @@ $(document).ready(function () {
         }
     });
 
+    // Nếu đang ở trang /search, gán các giá trị khác từ searchData
     if (window.location.pathname === '/search') {
-        const savedData = sessionStorage.getItem('searchData');
-        const searchData = savedData ? JSON.parse(savedData) : {};
+        if (searchData?.productName) {
+            $(".box-keysearch input").val(searchData.productName);
+        }
         getProductData(searchData);
     }
 
